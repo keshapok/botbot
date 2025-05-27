511626dcb43522bfb7915c0e6cbcade9cc222537
keshapok <165573971+keshapok@users.noreply.github.com>
2025-05-27 11:03:55 +0300

    Update MouseSimulator.cs

--- MouseSimulator.cs
+++ MouseSimulator.cs
@@ -1,29 +1,16 @@
 using System;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 public static class MouseSimulator
 {
     [DllImport("user32.dll", SetLastError = true)]
     private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, UIntPtr dwExtraInfo);
 
-    [DllImport("user32.dll")]
-    private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
-
-    [StructLayout(LayoutKind.Sequential)]
-    struct POINT
-    {
-        public int X;
-        public int Y;
-    }
-
-    [DllImport("user32.dll")]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    internal static extern bool GetCursorPos(ref POINT lpPoint);
-
     public enum MouseEventFlags : uint
     {
-        LEFTDOWN = 0x00000002,
-        LEFTUP = 0x00000004
+        LEFTDOWN = 0x0002,
+        LEFTUP = 0x0004
     }
 
     public static void MouseEvent(MouseEventFlags value)
